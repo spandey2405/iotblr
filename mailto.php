96949f01af305caca17288263fd49b6870925dc0
Saurabh Pandey <spandey2405@gmail.com>
2016-01-03 10:57:32 +0530

    Added Contact algo

--- mailto.php
+++ mailto.php
@@ -4,18 +4,35 @@ if($_POST) {
     $to = 'spandey2405@gmail.com';
     $name = $_POST['name'];
     $from = $_POST['email'];
-    $header = "From: $from" . "\r\n";
     $text= $_POST['message'];
     $subject = $_POST['subject'];
-    $html_text = "
-    <html>
-    <body>
-    <h1>Message From $name</h1>
-    <br>
-    $text
-    </body>";
+    $message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+// Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    mail($to,$subject,$html_text,$headers);
+// More headers
+    $headers .= "From: <spandey2405@gmail.com>" . "\r\n";
+    mail($to,$subject,$message,$headers);
     echo "Testing";
 }
 ?>
