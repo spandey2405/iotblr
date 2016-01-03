68c2f72a4313cbb199ad03b7054e33d870b983f7
Saurabh Pandey <spandey2405@gmail.com>
2016-01-03 10:50:46 +0530

    Added Contact algo

--- mailto.php
+++ mailto.php
@@ -3,11 +3,11 @@
 if($_POST) {
     $to = 'spandey2405@gmail.com';
     $name = $_POST['name'];
-    $header = $_POST['email'];
+    $header = "From: ".$_POST['email'];
     $text= $_POST['message'];
     $subject = $_POST['subject'];
     $text .= "<h1>Message From :".$name."</h1><br><br>".$text;
-    mail($to,$subject,$txt,$headers);
+    mail($to,$subject,$text,$headers);
     echo "Testing";
 }
 ?>
